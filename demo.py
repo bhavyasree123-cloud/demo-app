fae69c422eafa2cd0fcd792700833437da8de7cc
bhavyasree123-cloud <eddlapallibhavya01@gmail.com>
2025-12-09 13:41:42 -0600

    second commit

--- demo.py
+++ demo.py
@@ -4,15 +4,25 @@
 # print(c)
 
 #decorator function
-def my_decorator(func):
-    def inner():
-        print("Before function runs")
-        func()
-        print("After function runs")
-    return inner
+# def my_decorator(func):
+#     def inner():
+#         print("Before function runs")
+#         func()
+#         print("After function runs")
+#     return inner
 
-@my_decorator
-def say_hello():
-    print("Hello!")
+# @my_decorator
+# def say_hello():
+#     print("Hello!")
 
-say_hello()
+# say_hello()
+
+
+
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n - 1) + fib(n - 2)
+
+for i in range(10):
+    print(fib(i), end=" ")
